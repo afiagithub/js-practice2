82b3e7dd4c033b831183271587f48772830e628c
Afia Rahman <rahmanmou26@gmail.com>
2025-12-21 17:21:38 +0600

    added budget tracker (phase 1)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 // Example of using document.write()
 document.write("Afia Rahman <br/>")
 document.write("Student")
-document.write("<br/> Age: 24")
+document.write("<br/> Age: 24 <br/>")
 
 // typeof function
 console.log(typeof("Afia"))
 console.log(typeof(456.258))
 console.log(typeof(false))
 console.log(typeof(Rahman))
+
+// Budget tracker (Phase 1)
+console.log("My name is Maisha Raihana");
+console.log("Income: 15000, Budget: 12500")
+console.log("Rent: 9500 Food: 1500 <br/> Others: 1500") //break tag doesn't work in console.log
+
+
+// variable
+let firstName = "Afia"
+let lastName = "Rahman"
+document.write(firstName + " " + lastName)

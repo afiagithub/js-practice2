b4b73a8751bf792e1ebbe581b6105ef27af735c0
Afia Rahman <rahmanmou26@gmail.com>
2025-12-21 17:12:33 +0600

    Using typeof

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@
 document.write("Afia Rahman <br/>")
 document.write("Student")
 document.write("<br/> Age: 24")
+
+// typeof function
+console.log(typeof("Afia"))
+console.log(typeof(456.258))
+console.log(typeof(false))
+console.log(typeof(Rahman))

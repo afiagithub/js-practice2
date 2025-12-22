41ed72a2c94952199e4a1a898db87ca107211271
Afia Rahman <rahmanmou26@gmail.com>
2025-12-22 16:12:28 +0600

    Using conditional statements

--- js/calculator.js
+++ js/calculator.js
@@ -2,12 +2,12 @@
 // Arithmetic: +, -, *, /, %, **, ++, --
 // Assignment: =, +=, -=, *=, /=, %=, **=
 
-// let num1 = 10, num2 = 20;
-let num1 = prompt("Enter a number:");
-let num2 = prompt("Enter a number:");
+let num1 = 10, num2 = 20;
+// let num1 = prompt("Enter a number:");
+// let num2 = prompt("Enter a number:");
 
-num1 = parseInt(num1, 10)
-num2 = parseInt(num2, 10)
+// num1 = parseInt(num1, 10)
+// num2 = parseInt(num2, 10)
 
 let sum, sub, mul, div, rem, expo, inc, dec;
 
@@ -17,3 +17,24 @@ document.write("Mul: " + (num1*num2) + "<br/>")
 document.write("Div: " + (num1/num2) + "<br/>")
 document.write("Remainder: " + (num1%num2) + "<br/>")
 document.write("Exponent: " + (num2**num1) + "<br/>")
+
+// Area calculator
+
+// Triangle
+let base = 5, height = 8;
+let area = 0.5*base*height;
+
+console.log(area);
+
+// Temperature calculator
+let farhen = 125;
+let cel = (farhen - 32) * (5/9)
+console.log(cel.toFixed(2));
+
+// Budget tracker app (Phase 3)
+let income1 = 10000, budget1 = 5600;
+let balance, savings, tax;
+tax = income1*(10/100);
+balance = income1 - (tax + budget1)
+savings = balance*(20/100)
+console.log(`Balace is ${balance}, Savings is ${savings}`);
---
+++ js/conditional.js
@@ -0,0 +1,20 @@
+let mark = 56;
+
+if(mark>=80 && mark<=100){
+    console.log("A+");    
+}
+else if(mark>=70 && mark<80){
+    console.log("A");
+}
+else if(mark>=60 && mark<70){
+    console.log("A-");
+}
+else if(mark>=40 && mark<60){
+    console.log("B");
+}
+else if(mark>=33 && mark<40){
+    console.log("C");
+}
+else{
+    console.log("F");    
+}

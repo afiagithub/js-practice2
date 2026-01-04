7b277185ff0ba273127e77a58a3c636eb66c39b3
Afia Rahman <rahmanmou26@gmail.com>
2026-01-04 14:10:12 +0600

    used while loop

--- js/loop.js
+++ js/loop.js
@@ -1,11 +1,19 @@
-for(i=0; i<10; i= i+2){
+for(let i=0; i<10; i= i+2){
     document.write(" ", i)
 }
 
 let numSum=0;
 
-for(i=1; i<=10; i++){
+for(let i=1; i<=10; i++){
     numSum = numSum+i;    
 }
 
 document.write("<br>", numSum)
+
+let m = 2, evenSum = 0;
+while(m<=100){
+    evenSum+=m;
+    m+=2;
+}
+
+document.write("<br>", evenSum)

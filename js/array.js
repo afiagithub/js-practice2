97129f8cb7eefacf754b7800260d40f881081c6a
Afia Rahman <rahmanmou26@gmail.com>
2026-01-04 16:32:29 +0600

    used array element

---
+++ js/array.js
@@ -0,0 +1,7 @@
+let namList = ["Amit", "Kora", "Lily", "Joe", "Poppy"]
+namList.push("Nina")
+namList.pop();
+
+let numList = [1, 2, 3, 4, 5]
+namList = namList.concat(numList)
+console.log(namList);

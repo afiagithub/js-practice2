4cd08b509cc204293dc34aeef253a76535c9a621
Afia Rahman <rahmanmou26@gmail.com>
2026-01-04 14:18:16 +0600

    used do while loop

--- js/loop.js
+++ js/loop.js
@@ -7,7 +7,6 @@ let numSum=0;
 for(let i=1; i<=10; i++){
     numSum = numSum+i;    
 }
-
 document.write("<br>", numSum)
 
 let m = 2, evenSum = 0;
@@ -15,5 +14,21 @@ while(m<=100){
     evenSum+=m;
     m+=2;
 }
-
 document.write("<br>", evenSum)
+
+// Sum of numbers that are divisible by 3 and 5
+let n=1, taskSum=0;
+while(n<=100){
+    if(n%3==0 && n%5==0){
+        taskSum+=numSum;
+    }
+    n++;
+}
+document.write("<br>", taskSum)
+
+// do while loop executes once even if condition is false
+let p=50;
+do{
+    console.log(p);
+    p++;    
+}while(p<=20)

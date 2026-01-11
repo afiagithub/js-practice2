710db8150aa7678bfface638b177f61ba19f08f0
Afia Rahman <rahmanmou26@gmail.com>
2026-01-11 16:33:25 +0600

    added budget tracker (phase 6)

---
+++ js/dateObject.js
@@ -0,0 +1,8 @@
+let date = new Date();
+console.log(date);
+
+let newYr = date.getFullYear()
+console.log(newYr);
+
+let currMonth = date.getMonth();  //0 is Jan and 11 is Dec
+console.log(currMonth);
--- js/func.js
+++ js/func.js
@@ -1,4 +1,4 @@
-function add(num1, num2){
+function add(num1, num2) {
     let res = num1 + num2
     return res;
 }
@@ -7,13 +7,41 @@ let addRes = add(20, -30)
 console.log(addRes);
 
 // IIFE (Immediately Invokable Function Expression)
-(function disMsg(msg){
-    console.log(msg);    
+(function disMsg(msg) {
+    console.log(msg);
 })("Hello")
 
 // Function Expression
-const display2 = function displayMsg(msg){
-    console.log(msg);    
+const display2 = function displayMsg(msg) {
+    console.log(msg);
 }
 
 display2('Hello World')
+
+// Budget tracker (phase 6)
+// Function to get user input
+function getUserInput(promptMsg, isNumber = false) {
+    const userInput = prompt(promptMsg)
+    return isNumber ? parseFloat(userInput) : userInput
+}
+
+function budgetTracker() {
+    let numOfExp = parseInt(prompt("Enter number of expenses: "))
+    if (isNaN(numOfExp)) {
+        console.log("Invalid input");
+    }
+
+    let expArr = [], expSum = 0;
+
+    for (let i = 0; i < numOfExp; i++) {
+        let expense = getUserInput("Enter Expense: ", true)
+        if (isNaN(expense)) {
+            expense = 0;
+        }
+        expSum+=expense;
+        expArr.push(expense);
+    }
+    console.log(expArr, expSum);
+}
+
+budgetTracker();

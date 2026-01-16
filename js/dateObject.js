5a9009d7a7a8b0b7dbe1db0ba43d41eb72d4515e
Afia Rahman <rahmanmou26@gmail.com>
2026-01-16 23:08:46 +0600

    used more date methods

--- js/dateObject.js
+++ js/dateObject.js
@@ -6,3 +6,9 @@ console.log(newYr);
 
 let currMonth = date.getMonth();  //0 is Jan and 11 is Dec
 console.log(currMonth);
+
+let currDay = date.getDate()
+console.log(currDay);
+
+let currTIme = date.getMinutes()
+console.log(currTIme);

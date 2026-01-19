dbcf16bd7aef46417633eebb5020c35a58518ce7
Afia Rahman <rahmanmou26@gmail.com>
2026-01-19 20:43:18 +0600

    added dom manipulate

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+document.getElementById("hdr").innerHTML = "This is heading 1";
+
+const para = document.querySelectorAll(".mon")
+para[0].style.fontSize = "40px";
+
+const heading = document.querySelector("")

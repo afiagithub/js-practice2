aacfc12c208736a2d62c8d28470aa4b07315e5a4
Afia Rahman <rahmanmou26@gmail.com>
2026-01-19 20:48:47 +0600

    added queryselector

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,11 @@
 document.getElementById("hdr").innerHTML = "This is heading 1";
 
-const para = document.querySelectorAll(".mon")
+// queryselector - class = . | id = # | tag = only tag name
+const para = document.querySelectorAll(".mon") // selects all items with the same class name
 para[0].style.fontSize = "40px";
 
-const heading = document.querySelector("")
+const heading = document.querySelector("h2"); // selects the first item
+heading.style.color = "red"
+
+const listItem = document.querySelector("li a")
+listItem.style.color = "green"

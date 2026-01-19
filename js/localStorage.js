b8674c8007e996335f245b685722dfe1754a13ea
Afia Rahman <rahmanmou26@gmail.com>
2026-01-19 10:29:57 +0600

    added localstorage usage

---
+++ js/localStorage.js
@@ -0,0 +1,19 @@
+// Web storage API - allows us to store and read data in browser
+// Web storage API - localstorage, sessionstorage
+
+// localstorage - store, read, update and remove data. No expiary data
+// setItem(key, value)
+localStorage.setItem("userName", "Afia Rahman")
+localStorage.setItem("password", "12345")
+localStorage.setItem("Age", "25")
+
+const countries = ["Bangladesh", "Australia", "Spain", "Japan"]
+localStorage.setItem("countries", JSON.stringify(countries)) //for array list use json stringify
+
+// getItem(key)
+const userName = localStorage.getItem("userName")
+const countryList = JSON.parse(localStorage.getItem("countries")) //when getting array data use json parse
+console.log(userName, countryList);
+
+localStorage.removeItem("Age")
+localStorage.clear()
